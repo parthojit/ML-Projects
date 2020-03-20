b04fb2e86e089dfe3541c3fbc4b8392114d91048
Maria Alejandra Del Rio Denis <mariaalejandradelriodenis@Marias-MacBook-Air-1922.local>
2020-03-21 00:00:28 +0900

    Test commit

--- plotiris.py
+++ plotiris.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('https://archive.ics.uci.edu/ml/'
-        'machine-learning-databases/iris/iris.data', header=None)
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
 df.tail()
 
 
